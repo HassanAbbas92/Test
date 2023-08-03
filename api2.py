d5d3aaab7d3f61cb975ede367e747ffaa2e48d63
Hassan Ibrahim Abbas <32170019+HassanAbbas92@users.noreply.github.com>
2023-08-03 15:07:20 +0300

    Create api2.py

---
+++ api2.py
@@ -0,0 +1,28 @@
+from flask import Flask , render_template ,request , jsonify
+import os
+import io
+from PIL import Image
+import io, base64
+
+
+app = Flask(__name__)
+							
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    if request.method == 'POST':
+        img_bytes = request.form["image_file"]
+        print(img_bytes)
+        tall = float(request.form['Tall'])
+        print(tall)
+        img = Image.open(io.BytesIO(base64.decodebytes(bytes(img_bytes, "utf-8"))))
+        img.save('D:/Image/my-image.jpeg')
+        return jsonify({"request_id": tall})
+
+@app.route('/')
+def default():
+    return 'API Working'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
